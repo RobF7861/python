04dd4aa0c1675ce11bab61b8ebf11f07c49c6357
RobF7861 <rfernandez-ruiz@unomaha.edu>
2024-08-14 18:41:58 -0500

    Description and testing added

--- television.py
+++ television.py
@@ -1,28 +1,40 @@
 class Television:
-    min_volume = 0
-    max_volume = 2
-    min_channel = 0
-    max_channel = 3
+    min_volume:int = 0
+    max_volume:int = 2
+    min_channel:int = 0
+    max_channel:int = 3
 
     def __init__(self):
-        self.__status = False
-        self.__muted = False
-        self.__volume = self.min_volume
-        self.__channel = self.min_channel
+        '''
+        Creates a TV
+        '''
+        self.__status:bool = False
+        self.__muted:bool = False
+        self.__volume:int = self.min_volume
+        self.__channel:int = self.min_channel
 
     def __str__(self):
+        '''
+        Returns formatted string of TV conditions
+        '''
         if self.__muted:
             return f'Power = {self.__status}, Channel = {self.__channel}, Volume = 0'
         else:
             return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
 
     def power(self):
+        '''
+        Flips condition of TV power
+        '''
         if self.__status:
             self.__status = False
         elif not self.__status:
             self.__status = True
 
     def mute(self):
+        '''
+        Mutes/unmutes TV
+        '''
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -30,18 +42,31 @@ class Television:
                 self.__muted = True
 
     def channel_up(self):
+        '''
+        Increases channel value
+        Returns to the lowest value if channel value exceeds max value
+        '''
         if self.__status:
             self.__channel += 1
             if self.__channel > self.max_channel:
                 self.__channel = self.min_channel
 
     def channel_down(self):
+        '''
+        Decreases channel value
+        Returns to the highest value if channel value exceeds min value
+        '''
         if self.__status:
             self.__channel -= 1
             if self.__channel < self.min_channel:
                 self.__channel = self.max_channel
 
     def volume_up(self):
+        '''
+        Increases volume value
+        Unmutes TV
+        Value is set to max volume if max is exceeded
+        '''
         if self.__status:
             if self.__volume==self.max_volume:
                 self.__muted=False
@@ -52,6 +77,11 @@ class Television:
             pass
 
     def volume_down(self):
+        '''
+        Decreases volume value
+        Unmutes TV
+        Value is set to min volume if min is exceeded
+        '''
         if self.__status:
             if self.__volume==self.min_volume:
                 self.__muted = False
